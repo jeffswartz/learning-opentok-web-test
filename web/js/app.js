f3eb9aeb9d72f1849c781757d15e574a89cbbe40
Padmaja Peri <paddu.peri@gmail.com>
2015-01-06 10:52:56 -0800

    Incorporated Review Comments

--- web/js/app.js
+++ web/js/app.js
@@ -1,89 +1,86 @@
 //Declare Variables
 
 var apiKey,
-	sessionId,
-	token,
-	response,
-	session,
-	publisher,
-	archiveID,
-	url;
+    sessionId,
+    token,
+    session;
 
 
 //Initially hide the StopArchive and ViewArchive Buttons
-$(document).ready(function(){
-			$("#stop").hide();
-			archiveID = null;
+$(document).ready(function() {
 
-			getApiAndToken();
-			
-		});
+    getApiAndToken();
+
+});
 
 
-function getApiAndToken()
-{
-	$.get("/session",function(res){
+function getApiAndToken() {
+    $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res) {
 
-			apiKey = res.apiKey;
-            sessionId = res.sessionId;
-            token = res.token;
+        apiKey = res.apiKey;
+        sessionId = res.sessionId;
+        token = res.token;
 
-            initializeSession(); 
+        initializeSession();
 
-	});
+    });
 
 }
 
 
-                
-function initializeSession()
-{
-			//Initialize Session Object
-            session = OT.initSession(apiKey, sessionId);
+
+function initializeSession() {
+    //Initialize Session Object
+    session = OT.initSession(apiKey, sessionId);
 
 
 
-            //session Callback handlers
+    //session Callback handlers
 
 
-            session.on("streamCreated",function(event){
-        			$("#subscriber").append("<div id='subscriber_div'></div>");                              	       
-					session.subscribe(event.stream,"subscriber_div",{width:"100%",height:"100%"});
-            });  
+    session.on('streamCreated', function(event) {
 
-            
-
-            //Receive a message and append it to the history
-            var msgHistory = document.querySelector('#history');
-            session.on('signal:msg', function (event) {
-                     var msg = document.createElement('p');
-                     msg.innerHTML = event.data;
-                     msg.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
-                     msgHistory.appendChild(msg);
-                     msg.scrollIntoView();
-            }); 
+        session.subscribe(event.stream, 'subscriber', {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        });
+    });
 
 
-            //Connect to the Session
-            session.connect(token, function(error)
-            	{
 
-            		//If the connection is successful, initialize a publisher and publish to the session
-            		if(!error)
-            		{
-            			$("#publisher").append("<div id='publisher_div'></div>");
-                        publisher = OT.initPublisher("publisher_div",{width:"100%",height:"100%"});
-
-                        session.publish(publisher);
-                        
-            		}
-
-            	});
+    //Receive a message and append it to the history
+    var msgHistory = document.querySelector('#history');
+    session.on('signal:chat', function(event) {
+        var msg = document.createElement('p');
+        msg.innerHTML = event.data;
+        msg.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
+        msgHistory.appendChild(msg);
+        msg.scrollIntoView();
+    });
 
 
-                       
+    //Connect to the Session
+    session.connect(token, function(error) {
 
-}                  		
+        //If the connection is successful, initialize a publisher and publish to the session
+        if (!error) {
+
+            var publisher = OT.initPublisher('publisher', {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
+            });
+
+            session.publish(publisher);
+
+        }
+
+    });
+
+
+
+}
 
 //Text Chat
 
@@ -93,17 +90,15 @@ var msgTxt = document.querySelector('#msgTxt');
 
 
 //Send a signal once the user enters data in the form.This will send the data entered to all participants                      
-form.addEventListener('submit', function (event) {
-    	event.preventDefault();
-       
-     	session.signal({
-                			type: "msg",
-               				data: msgTxt.value
-                		}, function (error) {
-                       	if (!error) {
-                        	msgTxt.value = '';
-                		}
-        			}
-        );
-});             
-                        
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    session.signal({
+        type: 'chat',
+        data: msgTxt.value
+    }, function(error) {
+        if (!error) {
+            msgTxt.value = '';
+        }
+    });
+});
