6ce18dba2c1a455ae4f55064ec8646c8e985f4ff
Padmaja Peri <paddu.peri@gmail.com>
2015-01-06 10:18:04 -0800

    Incorporated Review Comments

--- web/js/app.js
+++ web/js/app.js
@@ -1,126 +1,109 @@
 //Declare Variables
-
 var apiKey,
-      sessionId,
-      token,
-      response,
-      session,
-      publisher,
-      archiveID,
-      status,
-      url;
+    sessionId,
+    token,
+    archiveID;
 
 
 //get the APIKEY and TOKEN 
-$(document).ready(function(){
+$(document).ready(function() {
 
-            $("#stop").hide();
-            archiveID = null;
+    $("#stop").hide();
+    archiveID = null;
 
-            getApiAndToken();
-                  
-            });
+    getApiAndToken();
+
+});
 
 
-function getApiAndToken()
-{
-      $.get("/session",function(res){
+function getApiAndToken() {
 
-            apiKey = res.apiKey;
-            sessionId = res.sessionId;
-            token = res.token;
+    $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res) {
 
-            initializeSession(); 
+        apiKey = res.apiKey;
+        sessionId = res.sessionId;
+        token = res.token;
 
-      });
+        initializeSession();
+
+    });
 
 }
 
 
-                
-function initializeSession()
-{
-                  //Initialize Session Object
-            session = OT.initSession(apiKey, sessionId);
+
+function initializeSession() {
+    //Initialize Session Object
+    var session = OT.initSession(apiKey, sessionId);
 
 
 
-            //CallBack Handlers
-            session.on("streamCreated",function(event){
-                        $("#subscriber").append("<div id='subscriber_div'></div>");                                      
-                              session.subscribe(event.stream,"subscriber_div",{width:"100%",height:"100%"});
-            });  
+    //Subscribe to a newly created stream
+    session.on('streamCreated', function(event) {
+        session.subscribe(event.stream, 'subscriber', {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        });
+    });
 
-            session.on("archiveStarted",function(event){
-                     archiveID = event.id;
+
+    //Handler for sessionDisconnected event
+    session.on('sessionDisconnected', function(event) {
+        console.log("The session got disconnected", event.reason);
+    });
+
+    session.on('archiveStarted', function(event) {
+        archiveID = event.id;
+    });
+
+
+    //Connect to the Session
+    session.connect(token, function(error) {
+
+        //If the connection is successful, initialize a publisher and publish to the session
+        if (!error) {
+            var publisher = OT.initPublisher('publisher', {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
             });
 
+            session.publish(publisher);
 
-            //Connect to the Session
-            session.connect(token, function(error)
-            {
+        } else {
 
-                        //If the connection is successful, initialize a publisher and publish to the session
-                        if(!error)
-                        {
-                              $("#publisher").append("<div id='publisher_div'></div>");
-                              publisher = OT.initPublisher("publisher_div",{width:"100%",height:"100%"});
+            console.log("There was an error connecting to the session", error.code, error.message);
+        }
 
-                              session.publish(publisher);
-                        
-                        }
-
-            });                  
+    });
 
 }
 
 
 
 //Start Archiving
-function startArchive()
-{
-                        
-        $.post("/start/"+sessionId);
-        $("#start").hide();
-        $("#stop").show();
+function startArchive() {
+
+    $.post(SAMPLE_SERVER_BASE_URL + "/start/" + sessionId);
+    $("#start").hide();
+    $("#stop").show();
 }
 
-                
+
 //Stop Archiving
-function stopArchive()
-{
-        $.post("/stop/"+archiveID);
-        $("#stop").hide();
-        $("#view").prop('disabled', false);
-} 
-
-        
-//Get the Archive Status. If it is  "available" then download it. Otherwise, keep checking every 5 secs till it is "available"
-function viewArchive()
-{
-        $("#view").prop("disabled", true);
-
-        $.post("/view/"+archiveID,function(res){
-      
-                status = res.status;
-                if(status=="available")
-                {
-                    downloadArchive(archiveID);
-                } else {
-                      window.setTImeout(viewArchive, 5000);
-                }
-        });
-  
-}       
+function stopArchive() {
+    $.post(SAMPLE_SERVER_BASE_URL + "/stop/" + archiveID);
+    $("#stop").hide();
+    $("#start").show();
+    $("#view").prop('disabled', false);
+}
 
 
-//Download Archive
-function downloadArchive(archiveID)
-{
-    window.location.href = "/download/"+archiveID;
-                 
+//View the Archive that was just created
+function viewArchive() {
+    $("#view").prop("disabled", true);
 
-        $("#start").show();
-        $("#view").prop('disabled', true);
-}                       
+    window.open(SAMPLE_SERVER_BASE_URL + "/view/" + archiveID);
 
+}
