6dae9c2cf6562a393bd8a7858d0bf5e66b78e6c2
Padmaja Peri <paddu.peri@gmail.com>
2015-01-07 15:39:44 -0800

    Formatted the code

--- web/js/app.js
+++ web/js/app.js
@@ -1,19 +1,16 @@
 //Declare Variables
-
 var apiKey,
     sessionId,
     token;
 
-
-//Initially hide the StopArchive and ViewArchive Buttons
+//get the APIKEY and TOKEN 
 $(document).ready(function() {
-
     getApiAndToken();
-
 });
 
 
 function getApiAndToken() {
+    //make an Ajax Request to get the apiKey, sessionId and token from the server
     $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res) {
 
         apiKey = res.apiKey;
@@ -21,24 +18,17 @@ function getApiAndToken() {
         token = res.token;
 
         initializeSession();
-
     });
 
 }
-
 
 
 function initializeSession() {
     //Initialize Session Object
     var session = OT.initSession(apiKey, sessionId);
 
-
-
-    //session Callback handlers
-
-
+    //Subscribe to a newly created stream
     session.on('streamCreated', function(event) {
-
         session.subscribe(event.stream, 'subscriber', {
             insertMode: 'append',
             width: '100%',
@@ -46,7 +36,28 @@ function initializeSession() {
         });
     });
 
+    //Handler for sessionDisconnected event
+    session.on('sessionDisconnected', function(event) {
+        console.log("The session got disconnected", event.reason);
+    });
 
+    //Connect to the Session
+    session.connect(token, function(error) {
+        //If the connection is successful, initialize a publisher and publish to the session
+        if (!error) {
+            var publisher = OT.initPublisher('publisher', {
+                insertMode: 'append',
+                width: '100%',
+                height: '100%'
+            });
+
+            session.publish(publisher);
+
+        } else {
+            console.log("There was an error connecting to the session", error.code, error.message);
+        }
+
+    });
 
     //Receive a message and append it to the history
     var msgHistory = document.querySelector('#history');
@@ -58,35 +69,12 @@ function initializeSession() {
         msg.scrollIntoView();
     });
 
-
-    //Connect to the Session
-    session.connect(token, function(error) {
-
-        //If the connection is successful, initialize a publisher and publish to the session
-        if (!error) {
-
-            var publisher = OT.initPublisher('publisher', {
-                insertMode: 'append',
-                width: '100%',
-                height: '100%'
-            });
-
-            session.publish(publisher);
-
-        }
-
-    });
-
-
-
 }
 
+
 //Text Chat
-
-
 var form = document.querySelector('form');
 var msgTxt = document.querySelector('#msgTxt');
-
 
 //Send a signal once the user enters data in the form.This will send the data entered to all participants                      
 form.addEventListener('submit', function(event) {
