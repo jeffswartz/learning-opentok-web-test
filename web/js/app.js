6c82c5356add11759de6a791485316ac2cfe407e
Padmaja Peri <paddu.peri@gmail.com>
2015-01-09 09:28:40 -0800

    Formatted code

--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,12 @@
-//Declare Variables
+// Declare Variables
 var apiKey,
     sessionId,
     token,
     archiveID;
 
-//get the APIKEY and TOKEN 
+// get the APIKEY and TOKEN 
 $(document).ready(function() {
-    $("#stop").hide();
+    $('#stop').hide();
     archiveID = null;
 
     getApiAndToken();
@@ -15,8 +15,8 @@ $(document).ready(function() {
 
 
 function getApiAndToken() {
-    //make an Ajax Request to get the apiKey, sessionId and token from the server
-    $.get(SAMPLE_SERVER_BASE_URL + "/session", function(res) {
+    // make an Ajax Request to get the apiKey, sessionId and token from the server
+    $.get(SAMPLE_SERVER_BASE_URL + '/session', function(res) {
 
         apiKey = res.apiKey;
         sessionId = res.sessionId;
@@ -29,10 +29,10 @@ function getApiAndToken() {
 
 
 function initializeSession() {
-    //Initialize Session Object
+    // Initialize Session Object
     var session = OT.initSession(apiKey, sessionId);
 
-    //Subscribe to a newly created stream
+    // Subscribe to a newly created stream
     session.on('streamCreated', function(event) {
         session.subscribe(event.stream, 'subscriber', {
             insertMode: 'append',
@@ -41,19 +41,19 @@ function initializeSession() {
         });
     });
 
-    //Handler for sessionDisconnected event
+    // Handler for sessionDisconnected event
     session.on('sessionDisconnected', function(event) {
-        console.log("The session got disconnected", event.reason);
+        console.log('You were disconnected from the session.', event.reason);
     });
 
-    //Handler for archiveStarted event
+    // Handler for archiveStarted event
     session.on('archiveStarted', function(event) {
         archiveID = event.id;
     });
 
-    //Connect to the Session
+    // Connect to the Session
     session.connect(token, function(error) {
-        //If the connection is successful, initialize a publisher and publish to the session
+        // If the connection is successful, initialize a publisher and publish to the session
         if (!error) {
             var publisher = OT.initPublisher('publisher', {
                 insertMode: 'append',
@@ -65,7 +65,7 @@ function initializeSession() {
 
         } else {
 
-            console.log("There was an error connecting to the session", error.code, error.message);
+            console.log('There was an error connecting to the session', error.code, error.message);
         }
 
     });
@@ -73,29 +73,29 @@ function initializeSession() {
 }
 
 
-//Start Recording
+// Start Recording
 function startArchive() {
-    //make an Ajax Request to start the recording
-    $.post(SAMPLE_SERVER_BASE_URL + "/start/" + sessionId);
-    $("#start").hide();
-    $("#stop").show();
+    // make an Ajax Request to start the recording
+    $.post(SAMPLE_SERVER_BASE_URL + '/start/' + sessionId);
+    $('#start').hide();
+    $('#stop').show();
 }
 
 
-//Stop Recording
+// Stop Recording
 function stopArchive() {
-    //make an Ajax Request to stop the recording
-    $.post(SAMPLE_SERVER_BASE_URL + "/stop/" + archiveID);
-    $("#stop").hide();
-    $("#start").show();
-    $("#view").prop('disabled', false);
+    // make an Ajax Request to stop the recording
+    $.post(SAMPLE_SERVER_BASE_URL + '/stop/' + archiveID);
+    $('#stop').hide();
+    $('#start').show();
+    $('#view').prop('disabled', false);
 }
 
 
-//View the Archive that was just created
+// View the Archive that was just created
 function viewArchive() {
-    //make an Ajax Request to view the archive
-    window.open(SAMPLE_SERVER_BASE_URL + "/view/" + archiveID);
-    $("#view").prop("disabled", true);
+    // make an Ajax Request to view the archive
+    window.open(SAMPLE_SERVER_BASE_URL + '/view/' + archiveID);
+    $('#view').prop('disabled', true);
 
 }
