5fba662e18bb2c3ec74e3f25d7a398dc3c5913bb
Jeff Swartz <swartz@tokbox.com>
2015-08-10 17:34:53 -0700

    White space clean-up.

--- web/js/app.js
+++ web/js/app.js
@@ -1,62 +1,41 @@
-// Declare Variables
-var apiKey,
-    sessionId,
-    token;
+var apiKey;
+var sessionId;
+var token;
 
-// get the APIKEY and TOKEN 
 $(document).ready(function() {
-    getApiAndToken();
+  // Make an Ajax Request to get the OpenTok API key, session ID and token
+  $.get(SAMPLE_SERVER_BASE_URL + '/session', function(res) {
+    apiKey = res.apiKey;
+    sessionId = res.sessionId;
+    token = res.token;
+    initializeSession();
+  });
 });
 
-
-function getApiAndToken() {
-    // make an Ajax Request to get the apiKey, sessionId and token from the server
-    $.get(SAMPLE_SERVER_BASE_URL + '/session', function(res) {
-
-        apiKey = res.apiKey;
-        sessionId = res.sessionId;
-        token = res.token;
-
-        initializeSession();
-    });
-
-}
-
-
 function initializeSession() {
-    // Initialize Session Object
-    var session = OT.initSession(apiKey, sessionId);
-
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function(event) {
-        session.subscribe(event.stream, 'subscriber', {
-            insertMode: 'append',
-            width: '100%',
-            height: '100%'
+  // Initialize Session object
+  var session = OT.initSession(apiKey, sessionId)
+    .on('streamCreated', function(event) {
+      subscriber = session.subscribe(event.stream, 'subscriber', {
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+      });
+    })
+    .on('sessionDisconnected', function(event) {
+      console.log('You were disconnected from the session.', event.reason);
+    })
+    .connect(token, function(error) {
+      // If the connection is successful, initialize a publisher and publish to the session
+      if (!error) {
+        publisher = OT.initPublisher('publisher', {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%'
         });
+        session.publish(publisher);
+      } else {
+        console.log('There was an error connecting to the session:', error.code, error.message);
+      }
     });
-
-    // Handler for sessionDisconnected event
-    session.on('sessionDisconnected', function(event) {
-        console.log('You were disconnected from the session.', event.reason);
-    });
-
-    // Connect to the Session
-    session.connect(token, function(error) {
-        // If the connection is successful, initialize a publisher and publish to the session
-        if (!error) {
-            var publisher = OT.initPublisher('publisher', {
-                insertMode: 'append',
-                width: '100%',
-                height: '100%'
-            });
-
-            session.publish(publisher);
-
-        } else {
-            console.log('There was an error connecting to the session:', error.code, error.message);
-        }
-
-    });
-
 }
